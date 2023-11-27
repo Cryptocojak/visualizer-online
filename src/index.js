038ad6a41691f5d180daf532bb719bef5462063a
cojak <cojak@email.local>
2023-11-27 01:40:13 -0800

    visualizer online day 3.51

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
-import { createRoot } from 'react-dom/client'; // Import createRoot here
+import { createRoot } from 'react-dom/client'; 
 import App from './App';
 
-// Get the root element where you want to render your app
 const rootElement = document.getElementById('root');
-const root = createRoot(rootElement); // Create a root
+const root = createRoot(rootElement); 
 
-// Render the App within ChakraProvider and StrictMode using the new root
 root.render(
   <React.StrictMode>
     <ChakraProvider>
