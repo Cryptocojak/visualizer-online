ebd64a00db6a95ec07d70e71a88424aeef794083
cojak <cojak@email.local>
2023-11-27 20:51:56 -0800

    visualizer online day 4

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { Button, Box, Center } from '@chakra-ui/react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
+import { Box, Center, Button, Text } from '@chakra-ui/react';
 import Web3 from 'web3';
 
 function App() {
   const [colour, setColor] = useState('#FFFFFF');
   const [colourHistory, setColorHistory] = useState(['#FFFF11', '#FF11FF', '#11FFFF']);
-  const web3 = new Web3(Web3.givenProvider || process.env.REACT_APP_INFURA_URL);
-  const [block, setBlock] = useState(web3.eth.getBlockNumber())
+  const [block, setBlock] = useState(null);
+  const [gasPrice, setGasPrice] = useState(null);
+  const [txCount, setTxCount] = useState(null);
 
-  function getRandomColor() {
+  const web3 = useRef(
+    new Web3(
+      new Web3.providers.HttpProvider(process.env.REACT_APP_INFURA_URL)
+    )
+  ).current;
+
+  const getRandomColor = () => {
     const letters = '0123456789ABCDEF';
     let newColor = '#';
     for (let i = 0; i < 6; i++) {
       newColor += letters[Math.floor(Math.random() * 16)];
     }
     return newColor;
-  }
+  };
 
   const handleChangeColour = useCallback(() => {
     const newColor = getRandomColor();
@@ -24,38 +31,49 @@ function App() {
   }, []);
 
   useEffect(() => {
-    let intervalId;
+    const fetchBlockData = async () => {
+      try {
+        const newBlockNumber = await web3.eth.getBlockNumber();
+        if (newBlockNumber !== block) {
+          setBlock(newBlockNumber);
+          handleChangeColour();
 
-    const checkBlockNumber = () => {
-      web3.eth.getBlockNumber()
-        .then(newBlockNumber => {
-          if (newBlockNumber !== block) {
-            setBlock(newBlockNumber);
-            handleChangeColour();
-          }
-        })
-        .catch(console.error);
+          const price = await web3.eth.getGasPrice();
+          setGasPrice(web3.utils.fromWei(price, 'gwei'));
+
+          const blockInfo = await web3.eth.getBlock(newBlockNumber);
+          setTxCount(blockInfo.transactions.length);
+        }
+      } catch (error) {
+        console.error('Error fetching block data:', error);
+      }
     };
 
-    intervalId = setInterval(checkBlockNumber, 7500);
-
+    fetchBlockData();
+    const intervalId = setInterval(fetchBlockData, 7500);
     return () => clearInterval(intervalId); 
-  }, [block, handleChangeColour, web3.eth]);
+  }, [handleChangeColour]); 
 
   const thirdLastColor = colourHistory[2];
 
   return (
     <Box bg={colour} minHeight="100vh">
-      <Center height="100vh">
+      <Center height="100vh" flexDirection="column">
         <Button
           colorScheme="teal"
           color="grey"
+          fontWeight="bold"
           bg={colourHistory[1]}
           _hover={{ bg: thirdLastColor }}
           onClick={handleChangeColour}
         >
           Change Colour (Work In Progress)
         </Button>
+
+        {/* Displaying the information */}
+        {block !== null && <Text color="grey" fontWeight="bold">Current Block: {block.toString()}</Text>}
+        {gasPrice !== null && <Text color="grey" fontWeight="bold">Current Gas Price: {parseFloat(gasPrice).toFixed(2)} Gwei</Text>}
+        {txCount !== null && <Text color="grey" fontWeight="bold">Transactions in Last Block: {txCount}</Text>}
       </Center>
     </Box>
   );
